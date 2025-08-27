39f9d56b67357ce75143ba904d86dc5fa8bd62d6
Todoya <joaohenrik2306@gmail.com>
2025-08-27 20:36:11 -0300

    ht6

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,1 +1,13 @@
 console.log(document.body);
+
+console.log(document.body.childNodes[1]);
+
+//selecionar por tag
+const ListaItens = document.getElementsByTagName('li')
+console.log(ListaItens);
+
+//para pegar o elemento btn e usar da escuta click
+const botao = document.getElementById('btn');
+botao.addEventListener('click', function() {
+    alert("Você clicou no botão")
+});
